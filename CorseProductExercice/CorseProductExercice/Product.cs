bbd4a9bf845abce696ae31954aa79d5e4648758b
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-03-14 09:50:29 -0300

    Product exercise

---
+++ CorseProductExercice/CorseProductExercice/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorseProductExercice {
+    internal class Product {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Quantidade * Preco;
+        }
+    }
+}
---
+++ CorseProductExercice/CorseProductExercice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+namespace CorseProductExercice {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product p = new Product();
+
+            Console.WriteLine("Entre os dados do produto");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture );
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+        }
+    }
+}
