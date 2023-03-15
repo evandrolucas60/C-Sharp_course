922546b06ab01d460b45f59000f43aa7388cea88
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-03-15 09:18:13 -0300

    add the ToString Method to product class

--- CorseProductExercice/CorseProductExercice/Product.cs
+++ CorseProductExercice/CorseProductExercice/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,17 @@ namespace CorseProductExercice {
 
         public double ValorTotalEmEstoque() {
             return Quantidade * Preco;
+        }
+
+        public override string ToString() {
+            return Nome 
+                + ", $ " 
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
         }
     }
 }
