51f251a71855c4228ded29d14df3eefcf07def80
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-17 16:52:30 -0300

    Modificador Out

---
+++ ModificadorOut/ModificadorOut/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadorOut {
+    //O modificador out é similar ao ref (faz o parâmetro ser uma referência para a variável
+    //original), mas não exige que a variável original seja iniciada.
+    class Calculator {
+        public static void Triple(int origin, out int result) {
+            result = origin * 3;
+        }
+    }
+}
---
+++ ModificadorOut/ModificadorOut/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadorOut {
+    class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
