44eee1007ff6ac6e5160829bacfc6ce03bbfc064
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-17 16:11:31 -0300

    solution using static methods

---
+++ ModificadorParams/ModificadorParams/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadorParams {
+    class Program {
+        static void Main(string[] args) {
+            int Sum1 = Calculator.Sum(2,3);
+            int Sum2 = Calculator.Sum(2,3,4);
+
+            Console.WriteLine(Sum1);
+            Console.WriteLine(Sum2);
+        }
+    }
+}
