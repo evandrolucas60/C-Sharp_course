8df5872a9cc40010a440b7004f3d029c116daa09
Evandro Silva <Evandro.silva@sup-arcoiris.local>
2023-03-17 14:43:28 -0300

    Array product class

---
+++ ArrayProduct/ArrayProduct/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProduct {
+    class Class1 {
+
+        public String nome { get; set; }
+        public int preco { get; set; }
+
+        public Class1() {
+        }
+
+        public Class1(string nome, int preco) {
+            this.nome = nome;
+            this.preco = preco;
+        }
+    }
+}
---
+++ ArrayProduct/ArrayProduct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProduct {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Class1[] vect = new Class1[n];
+
+            for (int i = 0; i < vect.Length; i++) {
+                string nome = Console.ReadLine();
+                int preco = int.Parse(Console.ReadLine());
+                vect[i] = new Class1(nome, preco);
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++) {
+                sum += vect[i].preco;
+            }
+
+            double avg = sum / n;
+            Console.WriteLine("THE AVERAGE IS: " + avg);
+        }
+    }
+}
