e772b4cdb3326711c39caa1c95dd0e997338a7b0
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-17 15:37:07 -0300

    Booking array exercice

---
+++ QuartoAluguelEcercicio/QuartoAluguelEcercicio/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartoAluguelEcercicio {
+    class Booking {
+
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public int Quarto { get; set; }
+
+        public override string ToString() {
+            return Quarto  
+                   + ": " 
+                   + Nome
+                   + ", "
+                   + Email;
+        }
+    }
+}
---
+++ QuartoAluguelEcercicio/QuartoAluguelEcercicio/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartoAluguelEcercicio {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Quantos quartos serão alugados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Booking[] vect = new Booking[n];
+
+            for (int i = 0; i < vect.Length; i++) {
+                Console.WriteLine("Aluguel #" + (i + 1));
+                Console.Write("Nome: ");
+                String Nome = Console.ReadLine();
+                Console.Write("Email: ");
+                String Email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int Quarto = int.Parse(Console.ReadLine());
+
+                vect[i] = new Booking { Nome = Nome, Email = Email, Quarto = Quarto };
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < vect.Length; i++) {
+                Console.WriteLine(vect[i]);
+            }
+
+
+
+        }
+    }
+}
