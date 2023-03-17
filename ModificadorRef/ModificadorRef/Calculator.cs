718a0b85413de083b6c007644c7d88e879d6baed
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-17 16:46:09 -0300

    Modificador ref

---
+++ ModificadorRef/ModificadorRef/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadorRef {
+    class Calculator {
+        //modificador ref faz o parâmetro ser uma refêrencia para a variável
+        public static void Triple(ref int x) {
+             x = x * 3;
+        }
+    }
+}
---
+++ ModificadorRef/ModificadorRef/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadorRef {
+    class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+        }
+    }
+}
