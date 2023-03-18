523c1614bb1c777eaa28bc9552ca28047b3e32f0
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-03-18 09:54:28 -0300

    Class List

---
+++ ListCSharp/ListCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace ListCSharp {
+    internal class Program {
+        static void Main(string[] args) {
+           
+            List<String> list = new List<String>();   
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anne");
+            list.Insert(2, "Marco");
+
+            foreach (String obj in list) { 
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine( "list count: " + list.Count);
+
+            String s1 = list.Find(Test);
+            Console.WriteLine("First 'A': " + s1);
+        }
+
+        static bool Test(String s) {
+            return s[0] == 'A';
+        }
+    }
+}
