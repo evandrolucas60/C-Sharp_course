5381b7b7691c5f7f8603d74b608fa55274c7ad57
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-22 16:01:07 -0300

    Sobreposição, palavra-chave Virtual, Override e base

---
+++ SobreposicaoKeywordVirtualOverrideBase/HerancaAccountExercice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HerancaAccountExercice.Entities;
+namespace HerancaAccountExercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Evandro", 500.00);
+            Account acc2 = new SavingsAccount(1002,"Lucas", 500.00, 0.01);
+
+            acc1.WithDraw(10);
+            acc2.WithDraw(10);
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
