f53e643ef450d5b05cd7ef989134d35af1be6f74
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-22 14:31:43 -0300

    inheritance

---
+++ HerancaAccountExercice/HerancaAccountExercice/Entities/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HerancaAccountExercice.Entities
+{
+    class Account
+    {
+
+        //variáveis de Instância
+        public int number { get; private set; }
+        public String Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+
+        //Construtores
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            this.number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //Métodos
+        public void WithDraw(double amount)
+        {
+            this.Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            this.Balance += amount;
+        }
+    }
+}
---
+++ HerancaAccountExercice/HerancaAccountExercice/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HerancaAccountExercice.Entities;
+namespace HerancaAccountExercice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account ac = new Account(1008, "Alex Green", 500.00);
+
+            Console.WriteLine(ac.Balance);
+        }
+    }
+}
