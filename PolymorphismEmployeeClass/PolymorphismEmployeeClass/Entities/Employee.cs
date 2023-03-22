edfc60679370744ada8aa95a3053d1aa183e06d1
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-22 16:56:42 -0300

    Polymorphism Employee class

---
+++ PolymorphismEmployeeClass/PolymorphismEmployeeClass/Entities/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismEmployeeClass.Entities
+{
+    class Employee
+    {
+        //Variáveis de instância
+        public String Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        //Construtores
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        //métodos
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+
+
+
+    }
+}
---
+++ PolymorphismEmployeeClass/PolymorphismEmployeeClass/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismEmployeeClass.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        //Variável de Instância
+        public double AdditionalCharge { get; set; }
+
+        //Construtores
+        public OutsourcedEmployee()
+        {
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge) 
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        //métodos
+        public sealed override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;
+        }
+
+
+    }
+}
