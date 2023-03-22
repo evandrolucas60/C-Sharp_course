b35843d7e7df44ca4dbccb2741f1f553816385d8
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-22 17:23:59 -0300

    Incomplete

--- PolymorphismEmployeeClass/PolymorphismEmployeeClass/Program.cs
+++ PolymorphismEmployeeClass/PolymorphismEmployeeClass/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PolymorphismEmployeeClass.Entities;
+using System.Globalization;
 
 namespace PolymorphismEmployeeClass
 {
@@ -10,6 +9,34 @@ namespace PolymorphismEmployeeClass
     {
         static void Main(string[] args)
         {
+            List<Employee> list = new List<Employee>();
+
+            Console.WriteLine("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Employee #{i + 1} data:");
+                Console.WriteLine("Outsourced (y/n)?");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine());
+
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double addCharge = double.Parse(Console.ReadLine());
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, addCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
 
         }
     }
