f0595e0423db13d908985f884b83581f99836076
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-23 13:22:36 -0300

    Polymorphism Employee class

--- PolymorphismEmployeeClass/PolymorphismEmployeeClass/Program.cs
+++ PolymorphismEmployeeClass/PolymorphismEmployeeClass/Program.cs
@@ -11,13 +11,14 @@ namespace PolymorphismEmployeeClass
         {
             List<Employee> list = new List<Employee>();
 
-            Console.WriteLine("Enter the number of employees: ");
+            Console.Write("Enter the number of employees: ");
             int n = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < n; i++)
             {
+                Console.WriteLine();
                 Console.WriteLine($"Employee #{i + 1} data:");
-                Console.WriteLine("Outsourced (y/n)?");
+                Console.Write("Outsourced (y/n)?");
                 char ch = char.Parse(Console.ReadLine());
                 Console.Write("Name: ");
                 String name = Console.ReadLine();
@@ -38,6 +39,13 @@ namespace PolymorphismEmployeeClass
                 }
             }
 
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+            foreach (Employee employee in list)
+            {
+                Console.WriteLine(employee.Name + "- $ "
+                        + employee.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
