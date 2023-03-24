4bca355b3d80f7738ac7cbf91d081a4edf5f4870
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-24 14:16:56 -0300

    try catch

---
+++ EstruturaTryCatch/EstruturaTryCatch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EstruturaTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                double result = (double) n1 / n2;
+                Console.WriteLine(result);
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format error!" + e.Message);
+            }     
+        }
+    }
+}
