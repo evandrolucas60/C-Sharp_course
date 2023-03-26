a58e6b9ac0026fdf35fce20e46b2e69ed869acd9
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-03-26 17:59:45 -0300

    try catch exercice

---
+++ TryCatchExercice/TryCatchExercice/Entities/Exception/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryCatchExercice.Entities.Exception
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(String msg)
+            : base(msg)
+        {
+        }
+    }
+}
---
+++ TryCatchExercice/TryCatchExercice/Program.cs
@@ -0,0 +1,42 @@
+﻿using TryCatchExercice.Entities;
+using TryCatchExercice.Entities.Exception;
+using System.Globalization;
+
+namespace TryCatchExercice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter account data: ");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            String holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double initialBalance = double.Parse(Console.ReadLine());
+            Console.Write("withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine());
+
+            Account account = new Account(number,holder,initialBalance,withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine());
+
+            try
+            {
+            account.Withdraw(amount);
+            Console.WriteLine("New balance: " + account.Balance);
+
+            }
+            catch (DomainException e)
+            {
+
+                Console.WriteLine("Error message: " + e.Message);
+            }
+
+
+        }
+    }
+}
