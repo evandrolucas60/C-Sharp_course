e7a829dc73d4099606ff0bb8750858638d0e5e37
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-03-26 20:11:28 -0300

    using block

---
+++ UsingBlockExample/UsingBlockExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace UsingBlockExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"C:\temp\file1.txt";
+
+
+            try
+            {
+                //garante que os objetos serão fechados ao termino do bloco using
+               
+                using (StreamReader sr = File.OpenText(path))//sitax simplificada
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        String line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.Write("An Error has occurred: ");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
