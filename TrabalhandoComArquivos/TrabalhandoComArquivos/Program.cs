597909ceda14a4e56fdbc0eeb47166e139e02b6d
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-03-26 19:35:18 -0300

    Working with files

---
+++ TrabalhandoComArquivos/TrabalhandoComArquivos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace TrabalhandoComArquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String sourcePath = @"C:\temp\file1.txt";
+            String targetPath = @"C:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                String[] lines = File.ReadAllLines(sourcePath);
+                foreach (String line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
