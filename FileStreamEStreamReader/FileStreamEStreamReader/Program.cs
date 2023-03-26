aa59567bfe5fe726ee60ca129ea0ff4319c0a52f
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2023-03-26 19:53:33 -0300

    FileStream and StreamReader

---
+++ FileStreamEStreamReader/FileStreamEStreamReader/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+namespace FileStreamEStreamReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"C:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+
+                String line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
