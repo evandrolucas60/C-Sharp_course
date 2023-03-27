3aa4e9139da51adbc4e9d2290cb375d1aaada957
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-27 13:55:42 -0300

    Directory e DirectoryInfo

---
+++ DiretoryEDiretoryInfo/DiretoryEDiretoryInfo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace DiretoryEDiretoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\myFolder";
+
+            try
+            {
+                //listar as pastas a partir de uma pasta informada
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (String s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                //listar os arquivos a partir de uma pasta informada
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (String f in files)
+                {
+                    Console.WriteLine(f);
+                }
+
+                //criar uma pasta
+                Directory.CreateDirectory(path + @"\newfolder");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has occurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
