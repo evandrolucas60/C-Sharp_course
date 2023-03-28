7bb9fdae9800d5b5bcf8c139a0194839aa5a2910
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-28 16:48:23 -0300

    First solution without IComparable

---
+++ InterfaceIComparable/InterfaceIComparable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace InterfaceIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"C:\temp\myFolder\file3.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<String> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort(); //ordenar a lista
+                    foreach (String str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has occurred: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
