b2c57f0c054cb280b97f852e84e399ecfb82ba81
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-29 16:39:03 -0300

    GetHashCode e Equals[C

---
+++ GetHashCodeEEqualsExemplo/GetHashCodeEEqualsExemplo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GetHashCodeEEqualsExemplo.Entities;
+
+namespace GetHashCodeEEqualsExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client {Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client {Name = "Lucas", Email = "Lucas@yahoo.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);//compara a referecia de memória
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
