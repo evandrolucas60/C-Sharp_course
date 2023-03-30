0cb608ef7a3438f3ca5fef5ad1c0570d2f1b4563
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-30 15:06:45 -0300

    Exercicio sobre Conjuntos

---
+++ ExercicioSobreConjuntosHashSetESortedSet/ExercicioSobreConjuntosHashSetESortedSet/Entities/LogRecord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioSobreConjuntosHashSetESortedSet.Entities
+{
+    class LogRecord
+    {
+        public String UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+
+            LogRecord otter = obj as LogRecord;
+            return UserName.Equals(otter.UserName);
+        }
+    }
+}
---
+++ ExercicioSobreConjuntosHashSetESortedSet/ExercicioSobreConjuntosHashSetESortedSet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ExercicioSobreConjuntosHashSetESortedSet.Entities;
+
+namespace ExercicioSobreConjuntosHashSetESortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>(); 
+
+            Console.Write("Enter file full path: ");
+            String path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        String[] line = sr.ReadLine().Split(' ');
+                        String name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { UserName = name, Instant = instant });
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.Write("An error has occurred:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
