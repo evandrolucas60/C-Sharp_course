385634b46e02e452fa6a83477e5bdd6fdea2b605
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-30 13:22:55 -0300

    HashSet e SortedSet

---
+++ HashSertESortedSet/HashSertESortedSet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSertESortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<String> set = new HashSet<String>();
+
+            set.Add("Tv");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Computer"));
+
+            foreach (String p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
