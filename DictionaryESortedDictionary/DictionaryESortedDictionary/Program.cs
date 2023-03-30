d143e2350b3145f21d6cb18acb13099c26ad2855
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-30 16:08:11 -0300

    Dictionary e SortedDicitionary

---
+++ DictionaryESortedDictionary/DictionaryESortedDictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryESortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, String> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99778855";
+            cookies["phone"] = "99990000";
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+
+            if (cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("There is no 'email' key");
+            }
+            Console.WriteLine("Size: " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES:");
+            foreach (KeyValuePair<String, String> item in cookies)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
