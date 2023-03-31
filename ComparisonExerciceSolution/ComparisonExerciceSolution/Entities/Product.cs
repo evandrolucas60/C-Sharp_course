2c98947f75c6058365a5b50b1439b9d1b1c799a2
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-31 15:41:38 -0300

    Comparison<t> delegate

--- ComparisonExerciceSolution/ComparisonExerciceSolution/Entities/Product.cs
+++ ComparisonExerciceSolution/ComparisonExerciceSolution/Entities/Product.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace ComparisonExerciceSolution.Entities
 {
-    class Product : IComparable<Product>
+    class Product
     {
         public String Name { get; set; }
         public double Price { get; set; }
@@ -14,15 +14,10 @@ namespace ComparisonExerciceSolution.Entities
             Price = price;
         }
 
-
         public override string ToString()
         {
             return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
         }
 
-        public int CompareTo(Product other)
-        {
-            return Name.ToUpper().CompareTo(other.Name.ToUpper());
-        }
     }
 }
--- ComparisonExerciceSolution/ComparisonExerciceSolution/Program.cs
+++ ComparisonExerciceSolution/ComparisonExerciceSolution/Program.cs
@@ -13,12 +13,19 @@ namespace ComparisonExerciceSolution
             products.Add(new Product("Notebook", 1200.00));
             products.Add(new Product("Tablet", 450.00));
 
-            products.Sort();//só funciona se a interface IComparable for implementada
+            //products.Sort();//só funciona se a interface IComparable for implementada
+
+            products.Sort(CompareProducts);
 
             foreach (var p in products)
             {
                 Console.WriteLine(p);
             }
+        }
+
+        static int CompareProducts(Product p1, Product p2)
+        {
+            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
     }
 }
