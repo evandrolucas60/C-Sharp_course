a26613aa51ccba9e685d4327097e6b3db3a14f30
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-31 14:13:19 -0300

    Extension Method

---
+++ ExtensionMethodsCsharp/ExtensionMethodsCsharp/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExtensionMethodsCsharp.Extensions
+{
+    static class DateTimeExtensions
+    {
+        public static String ElapsedTime(this DateTime thisObj )
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+            if (duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " Hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " Days";
+            }
+        }
+    }
+}
---
+++ ExtensionMethodsCsharp/ExtensionMethodsCsharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExtensionMethodsCsharp.Extensions;
+
+namespace ExtensionMethodsCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 3, 31, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
