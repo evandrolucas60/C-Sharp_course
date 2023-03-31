1464349cbd4ac03b22ca06956adf61d6d1af8936
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-31 15:19:11 -0300

    Usando Comparação IComparable - Primeira solução

---
+++ ComparisonExerciceSolution/ComparisonExerciceSolution/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using ComparisonExerciceSolution.Entities;
+
+namespace ComparisonExerciceSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product("TV", 900.00));
+            products.Add(new Product("Notebook", 1200.00));
+            products.Add(new Product("Tablet", 450.00));
+
+            products.Sort();//só funciona se a interface IComparable for implementada
+
+            foreach (var p in products)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
--- ExtensionMethodsCsharp/ExtensionMethodsCsharp/Program.cs
+++ ExtensionMethodsCsharp/ExtensionMethodsCsharp/Program.cs
@@ -12,7 +12,6 @@ namespace ExtensionMethodsCsharp
 
             String s1 = "Good morning dear students";
             Console.WriteLine(s1.Cut(10));
-           
         }
     }
 }
