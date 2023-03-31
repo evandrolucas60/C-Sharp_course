0804e734730b25fc685276c906342d755fcbcb83
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-31 17:05:59 -0300

    multcast delegate

---
+++ MultcastDelegates/MultcastDelegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MultcastDelegates.Services;
+
+namespace MultcastDelegates
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            //Guardam referência para mais de um método
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+        }
+    }
+}
