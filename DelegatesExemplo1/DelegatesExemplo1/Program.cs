47527c5f2172c498534c9009c391f3a258cf754e
evandrolucas60 <evandrolucas60@yahoo.com>
2023-03-31 16:53:28 -0300

    Delegate

---
+++ DelegatesExemplo1/DelegatesExemplo1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DelegatesExemplo1.Services;
+
+namespace DelegatesExemplo1
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
