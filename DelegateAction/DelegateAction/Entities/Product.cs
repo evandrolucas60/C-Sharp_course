762865b85aab361cfb866f8420b713d4ce83b612
evandrolucas60 <evandrolucas60@yahoo.com>
2023-04-03 14:16:09 -0300

    Delegate action

---
+++ DelegateAction/DelegateAction/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAction.Entities
+{
+    class Product
+    {
+        public String Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double prince)
+        {
+            Name = name;
+            Price = prince;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price;
+        }
+    }
+}
