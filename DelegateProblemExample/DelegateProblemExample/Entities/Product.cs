91fbbdd66d00736be03bf2b345890fea4794c591
evandrolucas60 <evandrolucas60@yahoo.com>
2023-04-03 15:42:04 -0300

    Exemplo Func

---
+++ DelegateProblemExample/DelegateProblemExample/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DelegateProblemExample.Entities
+{
+    class Product
+    {
+        public String Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ DelegateProblemExample/DelegateProblemExample/Service/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DelegateProblemExample.Entities;
+
+namespace DelegateProblemExample.Service
+{
+    class ProductService
+    {
+        public double FilteredSum(List<Product> list, Func<Product, bool> criteria)
+        {
+            double sum = 0.0;
+            foreach (Product p in list)
+            {
+                if (criteria.Invoke(p))
+                {
+                    sum += p.Price;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
