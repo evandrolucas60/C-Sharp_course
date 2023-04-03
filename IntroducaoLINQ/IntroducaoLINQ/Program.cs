ecc38ede21b2a5b078d93240a37ff7739b02a408
evandrolucas60 <evandrolucas60@yahoo.com>
2023-04-03 16:13:57 -0300

    LINQ Operation

---
+++ IntroducaoLINQ/IntroducaoLINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroducaoLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Spcify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Define the query expression
+            IEnumerable<int> result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => 10 * x);
+
+            //execute query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
