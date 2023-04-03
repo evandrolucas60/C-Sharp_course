3b4ff0138e125ee8b151e9ed6dc230c069eb9bdd
evandrolucas60 <evandrolucas60@yahoo.com>
2023-04-03 14:53:58 -0300

    Delegate Func

---
+++ DelegateFunc/DelegateFunc/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace DelegateFunc.Entities
+{
+    class Product
+    {
+        public String Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double prince)
+        {
+            Name = name;
+            Price = prince;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price;
+        }
+    }
+}
---
+++ DelegateFunc/DelegateFunc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using DelegateFunc.Entities;
+using System.Linq;
+
+namespace DelegateFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<String> result = list.Select(p => p.Name.ToUpper()).ToList();
+            foreach (String s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
