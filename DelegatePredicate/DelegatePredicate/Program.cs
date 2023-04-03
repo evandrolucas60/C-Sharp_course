593f6f1c52a5dcf1a116cc14beb9de1cc26fdeae
evandrolucas60 <evandrolucas60@yahoo.com>
2023-04-03 13:20:16 -0300

    Predicate with lambda expression

---
+++ DelegatePredicate/DelegatePredicate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using DelegatePredicate.Entities;
+using System.Globalization;
+
+namespace DelegatePredicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(p => p.Price >= 100.0);
+            foreach (Product product in list)
+            {
+                Console.WriteLine(product);
+            }
+        }
+    }
+}
