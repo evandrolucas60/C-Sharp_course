42b43d0bf23504a7fdf8afb2c34925d872aa84ca
evandrolucas60 <evandrolucas60@yahoo.com>
2023-04-04 14:33:06 -0300

    LINQ com notação SQL

---
+++ LINQNotacaoSimilarSQL/LINQNotacaoSimilarSQL/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LINQNotacaoSimilarSQL.Entities
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Tier { get; set; }
+    }
+}
