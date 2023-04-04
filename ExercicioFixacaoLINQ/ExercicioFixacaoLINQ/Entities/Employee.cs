2f05ebc4bc7bb58ed2eb08696dbb0357f6e338df
evandrolucas60 <evandrolucas60@yahoo.com>
2023-04-04 15:41:04 -0300

    LINQ Exercicios

---
+++ ExercicioFixacaoLINQ/ExercicioFixacaoLINQ/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacaoLINQ.Entities
+{
+    class Employee
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
---
+++ ExercicioLINQResolvido/ExercicioLINQResolvido/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+using ExercicioLINQResolvido.Entities;
+
+namespace ExercicioLINQResolvido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            String path = Console.ReadLine();
+
+            List<Product> products = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!(sr.EndOfStream))
+                {
+                    String[] fields = sr.ReadLine().Split(',');
+                    String name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    products.Add(new Product(name, price));
+                }
+            }
+
+            var avg = products.Select(p => p.Price).DefaultIfEmpty().Average();
+            Console.WriteLine("Average price: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var names = products.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (String name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
